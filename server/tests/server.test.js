87688989fa6321a41ea60c0f3a697853a43b0b7d
Mikko <m.a.pelkonen2@gmail.com>
2022-03-12 11:15:52 +0200

    Added new tests, Dockerfile

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,6 +2,8 @@ const supertest = require('supertest');
 const app = require('../app');
 
 const api = supertest(app);
+const checkprimeUrl = '/api/primes?action=checkprime';
+const sumandcheckUrl = '/api/primes?action=sumandcheck';
 
 test('server works', async () => {
   await api
@@ -30,24 +32,24 @@ test('nonsensical action is handled appropriately', async () => {
 
 test('checkprime works and returns json', async () => {
   await api
-    .get('/api/primes?action=checkprime&integer=7')
+    .get(`${checkprimeUrl}&integer=7`)
     .expect(200)
     .expect('Content-Type', /application\/json/);
 });
 
 test('checkprime returns correct json for prime', async () => {
-  const response = await api.get('/api/primes?action=checkprime&integer=7');
+  const response = await api.get(`${checkprimeUrl}&integer=7`);
   expect(response.body).toStrictEqual({ 'isPrime': true });
 });
 
 test('checkprime returns correct json for non-prime', async () => {
-  const response = await api.get('/api/primes?action=checkprime&integer=12');
+  const response = await api.get(`${checkprimeUrl}&integer=12`);
   expect(response.body).toStrictEqual({ 'isPrime': false });
 });
 
 test('checkprime malformed request is handled appropriately', async () => {
   const response = await api
-    .get('/api/primes?action=checkprime&integers=1,2,3')
+    .get(`${checkprimeUrl}&integers=1,2,3`)
     .expect(400)
     .expect('Content-Type', /application\/json/);
 
@@ -56,7 +58,26 @@ test('checkprime malformed request is handled appropriately', async () => {
 
 test('sumandcheck works and returns json', async () => {
   await api
-    .get('/api/primes?action=sumandcheck&integers=1,2,3')
+    .get(`${sumandcheckUrl}&integers=1,2,3`)
     .expect(200)
     .expect('Content-Type', /application\/json/);
+});
+
+test('sumandcheck returns correct json for prime sum', async () => {
+  const response = await api.get(`${sumandcheckUrl}&integers=10,10,10,10,20,20,20,1`);
+  expect(response.body).toStrictEqual({ 'result': 101, 'isPrime': true });
+});
+
+test('sumandcheck returns correct json for non-prime sum', async () => {
+  const response = await api.get(`${sumandcheckUrl}&integers=10,10,10,10,20,20,20`);
+  expect(response.body).toStrictEqual({ 'result': 100, 'isPrime': false });
+});
+
+test('sumandcheck malformed request is handled appropriately', async () => {
+  const response = await api
+    .get(`${sumandcheckUrl}&integer=111`)
+    .expect(400)
+    .expect('Content-Type', /application\/json/);
+
+  expect(response.body).toStrictEqual({ 'error': 'malformed request' });
 });
